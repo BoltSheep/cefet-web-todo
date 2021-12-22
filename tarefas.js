17b2771fb6d151726a6616f8a500e9db5b15f57f
BoltSheep <thidampe@gmail.com>
2021-12-21 23:56:59 -0300

    Terefas
    Tarefa 0,1,2 ok

---
+++ tarefas.js
@@ -0,0 +1,55 @@
+const tarefas = [{
+    nome: 'Fazer dever de Kotlin',
+    categoria: 'estudos',
+    realizada: false
+}, {
+    nome: 'Concertar DSlite',
+    categoria: 'lazer',
+    realizada: true
+}];
+
+function main() {
+    tarefas.forEach(element => insereTarefaNaPagina(element))
+}
+
+function insereTarefaNaPagina(objeto) {
+    let listaEl = document.querySelector('#lista-tarefas');
+    const li = document.createElement('li');
+    li.innerHTML = objeto.nome;
+    li.classList.add('item-tarefa');
+    if(objeto.realizada){
+        li.classList.add('marcado')
+    }
+    li.classList.add(`categoria-${objeto.categoria}`)
+    listaEl.appendChild(li);
+}
+
+function criarNovaTarefa() {
+    const novoNome = document.querySelector('#nova-tarefa-nome');
+    const novaCategoria = document.querySelector('#nova-tarefa-categoria');
+
+    const objeto = new Object();
+    objeto.nome = novoNome.value
+    objeto.categoria = novaCategoria.value 
+    objeto.realizada = false
+    insereTarefaNaPagina(objeto)
+    novoNome.value = '';
+    novoNome.focus();
+}
+
+function filtro(){
+    const novoFiltro = document.querySelector('#filtro-de-categoria');
+    let listaEl = document.querySelector('#lista-tarefas');
+    if (novoFiltro.value !== ""){
+        Object.keys(listaEl.children).forEach(element => {
+            console.log(element)
+            if (element.categoria !== novoFiltro.value){
+                element.classList.add('retido-no-filtro');
+            } else {
+                element.classList.remove('retido-no-filtro');
+            }
+        })
+    }
+}
+
+main();
